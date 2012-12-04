6603f866281a5c6178c6e4602df2cdde23f21fec
ktc <ktc@statsbiblioteket.dk>
2012-12-04 08:46:09 +0100

    Addition of bitrepository ingester

--- src/main/scripts/bitstorage.ingest.sh
+++ src/main/scripts/bitstorage.ingest.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+SCRIPT_PATH=$(dirname $(readlink -f $0))
 
-cd $(dirname $(readlink -f $0))
+FILENAME="$1"
+FILEPATH="$2"
+CHECKSUM="$3"
+LOCALFILEURL="file://${FILEPATH}"
+FILESIZE=$(stat -c%s $FILEPATH)
 
-ENTITY=$1
-LOCALFILE=$2
+source $SCRIPT_PATH/common.sh
 
 NAME=`basename $0 .sh`
 
-source common.sh
+APPDIR="${REKLAMEINGEST_HOME}/components/${reklameingest.bitrepository.ingester}"
 
-CMD="${REKLAMEINGEST_HOME}/components/${bitstorage.ingest}/bin/${bitstorage.ingest}.sh $LOCALFILE $CONFIGFILE"
+#dk.statsbiblioteket.medieplatform.bitrepository.ingester.Ingester \
+CMD="$JAVA_HOME/bin/java -cp $APPDIR/bin/*:$APPDIR/external-products/* \
+dk.statsbiblioteket.medieplatform.bitrepository.ingester.TheMockClient \
+$CONFIGFILE $LOCALFILEURL $FILENAME $CHECKSUM $FILESIZE"
 
 OUTPUT="`execute "$PWD" "$CMD" "$NAME" "$ENTITY"`"
 RETURNCODE=$?
