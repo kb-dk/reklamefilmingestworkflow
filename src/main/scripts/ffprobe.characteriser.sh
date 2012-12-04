cf203c2680600b0cb4c297370559b889617e9581
ktc <ktc@statsbiblioteket.dk>
2012-12-04 10:05:52 +0100

    Minor adjustment to ffprobe.characteriser.sh script

--- src/main/scripts/ffprobe.characteriser.sh
+++ src/main/scripts/ffprobe.characteriser.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-cd $(dirname $(readlink -f $0))
+SCRIPT_PATH=$(dirname $(readlink -f $0))
 
 ENTITY=$1
 LOCALFILE=$2
 
 NAME=`basename $0 .sh`
 
-source common.sh
+source $SCRIPT_PATH/common.sh
 
 CMD="${REKLAMEINGEST_HOME}/components/${ffprobe.characteriser}/bin/ffprobeCharacterise.sh $LOCALFILE $CONFIGFILE"
 
